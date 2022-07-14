762d82a6fc0fa30a59030f17be9d6f6df3b8cb01
tstanmay13 <tstanmay13@gmail.com>
2022-07-13 20:08:53 -0500

    added some search functionality

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const apiLink = "http://www.omdbapi.com/?apikey=93f3f842&";
 
-async function main(){
 
+
+async function searchAgain(event){
+    if (event.key === 'Enter') {
+        const searchKey = document.querySelector('#input__box').value;
+        localStorage.setItem("key", searchKey);
+        window.location.href = `${window.location.origin}/movies.html`
+    }
 }
 
+function search(){
+    const searchKey = document.querySelector('#input__box').value;
+    localStorage.setItem("key", searchKey);
+    // console.log(window);
+    window.location.href = `${window.location.origin}/movies.html`
+}
--- movies.js
+++ movies.js
@@ -1,30 +1,35 @@
-const apiLink = "http://www.omdbapi.com/?apikey=93f3f842&";
+// const apiLink = "http://www.omdbapi.com/?apikey=93f3f842&";
 const resultsListEl = document.querySelector(".results__container");
+const key = localStorage.getItem("key")
 
-async function main(){
-    const results = await fetch("https://www.omdbapi.com/?apikey=93f3f842&s=what");
+async function renderResults(search_key){
+    const results = await fetch(`https://www.omdbapi.com/?apikey=93f3f842&s=${search_key}`);
     const resultData = await results.json();
-    // resultsListEl.innerHTML = 
-
-    
+    console.log(resultData)
+    resultsListEl.innerHTML = resultData.Search.map((result) => getResultHTML(result)).join("");
 }
+renderResults(key);
+
+async function searchAgain(event){
+    if (event.key === 'Enter') {
+        const searchKey = document.querySelector('.input__box2').value;
+        renderResults(searchKey);
+    }
+}
+
 
 function getResultHTML(result){
     return `<div class="result">
     <div class="result--wrapper">
       <figure class="poster__wrapper">
-        <img src="./assets/poster.jpg" alt="" class="poster">
+        <img src="${result.Poster}" alt="" class="poster">
       </figure>
       <div class="result__description">
-        <p class="result__title"> Blade Runner </span>
-        <p class="result__year"> 2014 </p>
-        <p class="result__type"> Movie</p>
-        <p class="result__imdb"> tt0207201</p>
+        <p class="result__title"> ${result.Title} </span>
+        <p class="result__year"> ${result.Year} </p>
+        <p class="result__type"> ${result.Type}</p>
+        <p class="result__imdb"> ${result.imdbID}</p>
       </div>
     </div>
-  </div>
-    
-    
-    `
+  </div>`
 }
-main();
