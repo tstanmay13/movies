e60736788728003d9ad3d430048011c8dc3b5acf
tstanmay13 <tstanmay13@gmail.com>
2022-07-13 21:36:23 -0500

    animation

--- app.js
+++ app.js
@@ -16,3 +16,7 @@ function search(){
     // console.log(window);
     window.location.href = `${window.location.origin}/movies.html`
 }
+
+function gohome(){
+    window.location.href = `${window.location.origin}/index.html`
+}
